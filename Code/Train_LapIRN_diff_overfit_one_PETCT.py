7c002478066d8ef3fd745f904b58af66a46a00fe
koegl <koeglfryderyk@gmail.com>
2026-07-01 09:51:11 +0200

    start traiing levle 3 from best level 2

--- Code/Train_LapIRN_diff_overfit_one_PETCT.py
+++ Code/Train_LapIRN_diff_overfit_one_PETCT.py
@@ -4,8 +4,8 @@ import os
 os.environ["MLFLOW_ALLOW_FILE_STORE"] = "true"
 
 
-import level1
-import level2
+from pathlib import Path
+
 import level3
 import mlflow
 import my_data
@@ -60,13 +60,16 @@ def main() -> None:
             num_workers=config.num_workers,
         )
 
-        path_model_level1 = level1.train_lvl1(config, train_generator, valid_generator)
-        # print("skipping level 1, already trained")
-        # path_model_level1 = Path(
-        #     "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_stagelvl1_best.pth"
+        # path_model_level1 = level1.train_lvl1(config, train_generator, valid_generator)
+        # # print("skipping level 1, already trained")
+        # # path_model_level1 = Path(
+        # #     "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_stagelvl1_best.pth"
+        # # )
+        # path_model_level2 = level2.train_lvl2(
+        #     config, path_model_level1, train_generator, valid_generator
         # )
-        path_model_level2 = level2.train_lvl2(
-            config, path_model_level1, train_generator, valid_generator
+        path_model_level2 = Path(
+            "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_nimble-perch-653_stagelvl2_best.pth"
         )
         path_model_level3 = level3.train_lvl3(
             config, path_model_level2, train_generator, valid_generator
