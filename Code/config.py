4e2b3f4024ad3817f81a17abaa2cb588962f328c
koegl <koeglfryderyk@gmail.com>
2026-07-02 14:38:02 +0200

    start_channel_lvl1: int = 3

--- Code/config.py
+++ Code/config.py
@@ -51,7 +51,7 @@ class TrainingConfig:
     lr_lvl1: float = 1e-4
     lr_lvl2: float = 1e-3 * 0.5
     lr_lvl3: float = 1e-3 * 0.25
-    start_channel_lvl1: int = 11
+    start_channel_lvl1: int = 3
     start_channel_lvl2: int = 7
     start_channel_lvl3: int = 7
 
