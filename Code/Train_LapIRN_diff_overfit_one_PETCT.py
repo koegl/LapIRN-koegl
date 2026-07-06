3672814fd4b74fa6c8bf4c2b1c7744dfefc6e66b
koegl <koeglfryderyk@gmail.com>
2026-07-06 15:33:19 +0200

    train only level 3

--- Code/Train_LapIRN_diff_overfit_one_PETCT.py
+++ Code/Train_LapIRN_diff_overfit_one_PETCT.py
@@ -111,19 +111,19 @@ def main() -> None:
             #     valid_generator,
             # )
             # path_model_level_1 = paths_model_level1["best"]
-            path_model_level_1 = Path(
-                "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_shivering-midge-864_stagelvl1_best.pth"
-            )
-            paths_model_level2 = level2.train_lvl2(
-                config, path_model_level_1, train_generator, valid_generator
-            )
+            # path_model_level_1 = Path(
+            #     "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_shivering-midge-864_stagelvl1_best.pth"
+            # )
+            # paths_model_level2 = level2.train_lvl2(
+            #     config, path_model_level_1, train_generator, valid_generator
+            # )
             # path_model_level_2 = Path(
             #     "/home/iml/fryderyk.koegl/data/PSMAReg/models/PSMAReg_LapIRN_nosy-shrike-707_stagelvl2_best.pth"
             # )
-            # path_model_level_2 = Path(
-            #     "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_luminous-colt-866_stagelvl2_best.pth"
-            # )
-            path_model_level_2 = paths_model_level2["best"]
+            path_model_level_2 = Path(
+                "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_unleashed-auk-838_stagelvl2_best.pth"
+            )
+            # path_model_level_2 = paths_model_level2["best"]
             path_model_level3 = level3.train_lvl3(
                 config, path_model_level_2, train_generator, valid_generator
             )
