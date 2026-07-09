876c6976683faca1a946c621fbe6278520e3f1d3
koegl <koeglfryderyk@gmail.com>
2026-07-09 11:29:55 +0200

    reduce lr

--- Code/config.py
+++ Code/config.py
@@ -54,7 +54,7 @@ class TrainingConfig:
     n_classes: int = 3
     lr_lvl1: float = 5e-4
     lr_lvl2: float = 5e-4
-    lr_lvl3: float = 1e-4
+    lr_lvl3: float = 1e-5
     start_channel: int = 11
 
     # train val
