d5aed9dcb631a29def3f7109570abf9e48a88c6a
koegl <koeglfryderyk@gmail.com>
2026-07-10 17:20:23 +0200

    imporve overfti checklist

--- Code/overfit_checklist.py
+++ Code/overfit_checklist.py
@@ -1,6 +1,6 @@
 """
 augmentaiton is off
-validation is off
+validation is off - both in config and at the end of train
 saving cases in training
 redcued datset sizes to 1
 choose between either synthetic or real data
