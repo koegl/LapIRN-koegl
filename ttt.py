727dda300bf7dbf9a68a0dd6e2800d84dea74aa8
koegl <koeglfryderyk@gmail.com>
2026-07-31 09:34:02 +0200

    t

--- ttt.py
+++ ttt.py
@@ -1,34 +1,37 @@
+import shutil
 from pathlib import Path
 
-dir_rib = Path("/home/iml/fryderyk.koegl/data/PET_CT_bone/segmentations_ribs")
+import numpy as np
+
+dir_rib = Path("/home/iml/fryderyk.koegl/data/temp")
 
 dir_pet_labels = Path("/home/iml/fryderyk.koegl/data/PET_CT_bone/pet_labels")
-pet_label_patients = [p.name for p in sorted(dir_pet_labels.iterdir()) if p.is_dir()]
+patients_pet_labels = [p.name for p in sorted(dir_pet_labels.iterdir()) if p.is_dir()]
 dir_total_labels = Path(
     "/home/iml/fryderyk.koegl/data/PET_CT_bone/segmentations_total_segmentator"
 )
-total_label_patients = [
+patients_totalsegmentator = [
     p.name for p in sorted(dir_total_labels.iterdir()) if p.is_dir()
 ]
 
+patients_ribs = [p.name for p in sorted(dir_rib.iterdir()) if p.is_dir()]
 
-not_in_pet = []
-not_in_total = []
+patient_in_all_3 = list(
+    set(patients_pet_labels) & set(patients_totalsegmentator) & set(patients_ribs)
+)
 
-for patient in sorted(dir_rib.iterdir()):
-    if patient.name not in pet_label_patients:
-        not_in_pet.append(patient.name)
-    if patient.name not in total_label_patients:
-        not_in_total.append(patient.name)
+# shuffle the list of patients
+np.random.seed(42)
+np.random.shuffle(patient_in_all_3)
 
-x = 0
+selected = patient_in_all_3[:20]
+path_destination_ribs = Path(
+    "/home/iml/fryderyk.koegl/data/PET_CT_bone/segmentations_ribs/"
+)
 
-print("missing pet labels:")
-for p in not_in_pet:
-    print(p)
-
-print("\n\nmissing total labels:")
-for p in not_in_total:
-    print(p)
+for patient in selected:
+    source_path = dir_rib / patient
+    destination_path = path_destination_ribs / patient
+    shutil.copytree(source_path, destination_path)
 
 x = 0
