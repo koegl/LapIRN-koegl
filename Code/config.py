ce2bfd36708563ba1482eabed136200d75bb57ac
koegl <koeglfryderyk@gmail.com>
2026-08-06 18:15:46 +0200

    similairyt to ncc

--- Code/config.py
+++ Code/config.py
@@ -1,7 +1,7 @@
 import socket
 from dataclasses import asdict, dataclass, field
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Literal, Optional, Tuple
 
 COMPUTER_NAME = socket.gethostname()
 
@@ -198,7 +198,7 @@ class TrainingConfig:
     # (MIND-SSC descriptor MSE, uses mind_radius / mind_dilation below).
     # NB: MIND is a plain MSE in [0, 1] while NCC is in [-1, 0], so w_ct / w_pet
     # have to be re-tuned when switching.
-    similarity_metric: str = "ssd"
+    similarity_metric: Literal["ncc", "mind"] = "ncc"
     mind_radius: int = 2
     mind_dilation: int = 2
 
