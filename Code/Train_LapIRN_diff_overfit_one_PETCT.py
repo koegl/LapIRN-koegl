afa77e0d596f950071544f50235b353e4554dc60
koegl <koeglfryderyk@gmail.com>
2026-08-19 17:20:11 +0200

    24 channel run

--- Code/Train_LapIRN_diff_overfit_one_PETCT.py
+++ Code/Train_LapIRN_diff_overfit_one_PETCT.py
@@ -151,26 +151,11 @@ def main() -> None:
                 # path_model_level_2 = paths_model_level2["best"]
                 # path_model_level_2 = paths_model_level2["final"]
                 print("skipping level 2, already trained")
-                path_model_level_2 = Path(
-                    "/home/iml/fryderyk.koegl/data/PSMAReg/models/PSMAReg_LapIRN_merciful-shrike-local_stagelvl2_best.pth"
-                )
                 # path_model_level_2 = Path(
-                #     "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_gentle-rat-39450513_stagelvl2_best.pth"
+                #     "/home/iml/fryderyk.koegl/data/PSMAReg/models/PSMAReg_LapIRN_merciful-shrike-local_stagelvl2_best.pth"
                 # )
-                # path_model_level3 = level3.train_lvl3(
-                #     config,
-                #     path_model_level_2,
-                #     train_generator,
-                #     valid_generator,
-                #     valid_tubingen_generator,
-                #     valid_nlst_generator,
-                #     valid_abdomen_generator,
-                # )
-                resume_model_path_lvl3 = Path(
-                    "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_angry-mare-39459564_stagelvl3_best_combined.pth"
-                )
-                resume_optimizer_path_lvl3 = Path(
-                    "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_angry-mare-39459564_stagelvl3_best_combined_optimizer.pth"
+                path_model_level_2 = Path(
+                    "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_gentle-rat-39450513_stagelvl2_best.pth"
                 )
                 path_model_level3 = level3.train_lvl3(
                     config,
@@ -180,9 +165,24 @@ def main() -> None:
                     valid_tubingen_generator,
                     valid_nlst_generator,
                     valid_abdomen_generator,
-                    resume_model_path=resume_model_path_lvl3,
-                    resume_optimizer_path=resume_optimizer_path_lvl3,
                 )
+                # resume_model_path_lvl3 = Path(
+                #     "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_angry-mare-39459564_stagelvl3_best_combined.pth"
+                # )
+                # resume_optimizer_path_lvl3 = Path(
+                #     "/lustre/groups/iml/data/PSMAReg/models/PSMAReg_LapIRN_angry-mare-39459564_stagelvl3_best_combined_optimizer.pth"
+                # )
+                # path_model_level3 = level3.train_lvl3(
+                #     config,
+                #     path_model_level_2,
+                #     train_generator,
+                #     valid_generator,
+                #     valid_tubingen_generator,
+                #     valid_nlst_generator,
+                #     valid_abdomen_generator,
+                #     resume_model_path=resume_model_path_lvl3,
+                #     resume_optimizer_path=resume_optimizer_path_lvl3,
+                # )
 
         # print(f"Final model path: {path_model_level3}")
 
